fc14d8f8792039317aedfc55ae4c2bcd7ac48bc9
speedytubepills <speedytubepills@gmail.com>
2024-07-30 15:28:35 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js"
+import { version } from "react";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const versionQuery = await database.query("SHOW server_version;");
+  const dbVersion = versionQuery.rows[0].server_version
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB
+  const openedConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const openedConnections = openedConnectionsQuery.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      }
+    }
+  });
 }
 
 export default status;
