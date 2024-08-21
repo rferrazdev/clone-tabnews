bea158483dc93fde6e4459bbe328a31fbed3da4c
speedytubepills <speedytubepills@gmail.com>
2024-08-21 22:23:15 +0000

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,16 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 import { version } from "react";
 
 async function status(req, res) {
   const updatedAt = new Date().toISOString();
 
   const versionQuery = await database.query("SHOW server_version;");
-  const dbVersion = versionQuery.rows[0].server_version
+  const dbVersion = versionQuery.rows[0].server_version;
 
   const maxConnectionsQuery = await database.query("SHOW max_connections;");
   const maxConnections = maxConnectionsQuery.rows[0].max_connections;
 
-  const dbName = process.env.POSTGRES_DB
+  const dbName = process.env.POSTGRES_DB;
   const openedConnectionsQuery = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [dbName],
@@ -24,8 +24,8 @@ async function status(req, res) {
         version: dbVersion,
         max_connections: parseInt(maxConnections),
         opened_connections: openedConnections,
-      }
-    }
+      },
+    },
   });
 }
 
