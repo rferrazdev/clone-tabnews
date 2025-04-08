71bd508a551accd27bdfc183521fff48ec51b464
speedytubepills <speedytubepills@gmail.com>
2025-04-08 20:00:50 +0000

    feat: create and use `InternalServerError`in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,31 +1,40 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 async function status(req, res) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const versionQuery = await database.query("SHOW server_version;");
-  const dbVersion = versionQuery.rows[0].server_version;
+    const versionQuery = await database.query("SHOW server_version;");
+    const dbVersion = versionQuery.rows[0].server_version;
 
-  const maxConnectionsQuery = await database.query("SHOW max_connections;");
-  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+    const maxConnectionsQuery = await database.query("SHOW max_connections;");
+    const maxConnections = maxConnectionsQuery.rows[0].max_connections;
 
-  const dbName = process.env.POSTGRES_DB;
-  const openedConnectionsQuery = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [dbName],
-  });
-  const openedConnections = openedConnectionsQuery.rows[0].count;
+    const dbName = process.env.POSTGRES_DB;
+    const openedConnectionsQuery = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [dbName],
+    });
+    const openedConnections = openedConnectionsQuery.rows[0].count;
 
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: dbVersion,
-        max_connections: parseInt(maxConnections),
-        opened_connections: openedConnections,
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: dbVersion,
+          max_connections: parseInt(maxConnections),
+          opened_connections: openedConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObj = new InternalServerError({
+      cause: error,
+    });
+    console.error("Error retrieving status:", publicErrorObj);
+    res.status(500).json(publicErrorObj);
+  }
 }
 
 export default status;
